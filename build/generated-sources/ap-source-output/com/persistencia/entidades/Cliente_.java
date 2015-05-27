e10ae81d18e8db0d4d1211dc4710dfe1508ffe82
jhmera <jjmebur@gmail.com>
2015-05-26 21:18:40 -0500

    se coloca primface de seleccion de nodos y login
    login y primface de seleccion de nodos

--- build/generated-sources/ap-source-output/com/persistencia/entidades/Cliente_.java
+++ build/generated-sources/ap-source-output/com/persistencia/entidades/Cliente_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-05-21T11:03:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-05-26T20:48:09")
 @StaticMetamodel(Cliente.class)
 public class Cliente_ { 
 
